e2e8329d0f363dad30a366d04ea13fefb259bb44
darya ryazanova <d.riazanova@mail.ru>
2024-02-05 19:33:01 +0300

    4 задача

--- 1.py
+++ 1.py
@@ -43,6 +43,11 @@ def promotion_image():
     <html lang="ru">
         <head>
             <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet" 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+            crossorigin="anonymous">
             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
             <title>Привет, Марс!</title>
         </head>
@@ -50,9 +55,14 @@ def promotion_image():
         <h1>Жди нас, Марс!</h1>
         <img src="{url_for('static', filename='img/MARS.png')}"
                alt="здесь должна была быть картинка, но не нашлась">
-        <h4>Вот она какая, красная планета.</h4>
+        <div class="alert alert-dark" role="alert">Человечество вырастает из детства.</div>
+        <div class="alert alert-success" role="alert">Человечеству мала одна планета.</div>
+        <div class="alert alert-light" role="alert">Мы сделаем обитаемыми безжизненные пока планеты.</div>
+        <div class="alert alert-warning" role="alert">И начнем с Марса!</div>
+        <div class="alert alert-danger" role="alert">Присоединяйся!</div>
     </body>
     </html>'''
+
 
 
 if __name__ == '__main__':
